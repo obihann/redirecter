d82db7e349d402c58df40ec78f1546e488fd0729
Jeff Hann <jeff.hann@redspace.com>
2016-04-01 15:24:04 -0300

    adding multithreading

--- redirecter/redirecter.py
+++ redirecter/redirecter.py
@@ -1,10 +1,30 @@
+import threading
+import time
+from queue import Queue
+
 from redirecter.models.page import Page 
 from redirecter.models.site import Site
 
 class Redirecter(object):
     def __init__(self):
-        self._outputLevel = 1
+        self._outputLevel = 2
+        self._q = Queue()
         self._site = Site(self._outputLevel)
+
+        for i in range(8):
+            t = threading.Thread(target=self._worker)
+            t.daemon = True
+            t.start()
+        
+
+    def _worker(self):
+        while True:
+            item = self._q.get()
+
+            item.scan()
+            self._addPages(item.links)
+
+            self._q.task_done()
     
     # create new site and page from link
     # scan page for links and add page to site
@@ -12,8 +32,10 @@ class Redirecter(object):
     def loadSite(self, home):
         self._home = home
         self._site[self._home] = Page(self._home)
-        self._site[self._home].scan()
-        self._addPages(self._site[self._home].links)
+
+        start = time.perf_counter()
+        self._q.put(self._site[self._home])
+        self._q.join()
 
         return self._site
 
@@ -23,8 +45,20 @@ class Redirecter(object):
     def _scanSite(self):
         for x in self._site:
             if not self._site[x].links:
-                self._site[x].scan()
-                self._addPages(self._site[x].links)
+                self._q.put(self._site[x])
+
+    def _scanPage(self, link, childPages = False):
+        if link not in self._site:
+            page = Page(link)
+            page.scan()
+
+            if childPages:
+                for x in page.links:
+                    if x not in self._site: 
+                        self._site[x] = Page(x)
+                        return True
+
+        return False
 
     # loop through all links
     # check if links are already pages
@@ -33,14 +67,6 @@ class Redirecter(object):
         scan = False
 
         for link in links:
-            if link not in self._site:
-                page = Page(link)
-                page.scan()
-
-                if childPages:
-                    for x in page.links:
-                        if x not in self._site: 
-                            self._site[x] = Page(x)
-                            scan = True
+            scan = self._scanPage(link, childPages)
 
         if scan: self._scanSite()
